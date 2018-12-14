03714e7b1fe70018a4d463ff300cea9c2046f217
maddyking44 <madelineking44@gmail.com>
2018-12-14 19:09:31 +1300

    npm brew yarn babel and viro updated, everything broke

--- ViroSample/js/ARPortals/MainScene.js
+++ ViroSample/js/ARPortals/MainScene.js
@@ -26,21 +26,21 @@ var MainScene = createReactClass({
       <ViroAmbientLight color="#ffffff" intensity={200}/>
         <ViroPortalScene passable={true} dragType="FixedDistance" onDrag={()=>{}}>
           <ViroPortal position={[0, 0, -1]} scale={[.1, .1, .1]}>
-            <Viro3DObject source={require('./portal_res/portal_ship/portal_ship.vrx')}
+            <Viro3DObject source={require('/Users/maddystar/maddy_workspace/AR_Viro_portal/ViroSample/js/ARPortals/asset.vrx')}
               resources={[require('./portal_res/portal_ship/portal_ship_diffuse.png'),
                           require('./portal_res/portal_ship/portal_ship_normal.png'),
                           require('./portal_res/portal_ship/portal_ship_specular.png')]}
               type="VRX"/>
           </ViroPortal>
           <Viro360Image source={require("./portal_res/360_island.jpg")} />
-                <ViroPortal position={[0, 0, -1]} scale={[.1, .1, .1]}>
+                {/* <ViroPortal position={[0, 0, -1]} scale={[.1, .1, .1]}>
                     <Viro3DObject source={require('./portal_res/portal_ship/portal_ship.vrx')}
                     resources={[require('./portal_res/portal_ship/portal_ship_diffuse.png'),
                                 require('./portal_res/portal_ship/portal_ship_normal.png'),
                                 require('./portal_res/portal_ship/portal_ship_specular.png')]}
                     type="VRX"/>
                 </ViroPortal>
-                <Viro360Image source={require("./portal_res/360_island.jpg")} />
+                <Viro360Image source={require("./portal_res/360_island.jpg")} /> */}
         </ViroPortalScene>
       </ViroARScene>
     );
